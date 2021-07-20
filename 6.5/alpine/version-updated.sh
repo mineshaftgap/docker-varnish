aa3751000682f06039090fd21baed1b46bf016a0
Kenn Herman <kenn@ifsight.com>
2021-07-20 09:43:02 -0700

    stdout/err needed redirection

--- 6.5/alpine/version-updated.sh
+++ 6.5/alpine/version-updated.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-apk del varnish 2>&1 /dev/null
-apk add --no-cache varnish 2>&1 /dev/null
+apk del varnish 2>&1 > /dev/null
+apk add --no-cache varnish 2>&1 > /dev/null
 varnishd -V 2>&1|grep -o -E 6.5.[0-9]+
